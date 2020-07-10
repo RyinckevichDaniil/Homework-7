ea4518cb4e0914753a31fed53fb62dbc371f8469
RyinckevichDaniil <abdul.alahakbar@gmail.com>
2020-07-10 20:31:43 +0300

    Done

---
+++ src/index.js
@@ -0,0 +1,52 @@
+function odd(arr) {
+    let result = [];
+    for (let index = 0; index < arr.length; index++) {
+        if(arr[i] % 2 == 1) {
+            result.push(arr[index]);
+        } else {
+            continue;
+        }
+        
+    }
+    return result;
+}
+
+function even(arr) {
+    let result = [];
+    for (let index = 0; index < arr.length; index++) {
+        if(arr[index] % 2 == 0) {
+            result.push(arr[i]);
+        } else {
+            continue;
+        }
+        
+    }
+    return result;
+}
+
+function flat(arr) {
+    result = [];
+    for (let index = 0; index < arr.length; index++) {
+
+        if(typeof arr[index] === 'number') {
+            result.push(arr[index])
+
+        } else {
+
+            for (let j = 0; j < arr[index].length; j++) {
+                result.push(arr[index][j]);
+
+            }
+        }
+    }
+    return result;
+}
+
+const array =[
+
+    [3,2,6], 4, 5, [9, 7, 10], 11
+
+    ]
+
+
+console.log(flat(array));
